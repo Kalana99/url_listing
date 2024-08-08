b0d5b096f876ad0d7342845de26744603d13d41a
Kalana Rubasinghe <kalanarajika99@gmail.com>
2024-08-08 10:53:37 +0530

    crawler

---
+++ ExcelReader.py
@@ -0,0 +1,21 @@
+import pandas as pd
+
+class ExcelReader:
+    
+    @staticmethod
+    def read_web():
+        return ExcelReader.read_excel_file("client_domains_000.xlsx")
+    
+    @staticmethod
+    def read_news():
+        return ExcelReader.read_excel_file("utils/news.xlsx")
+    
+    @staticmethod
+    def read_excel_file(file_path):
+
+        try:
+            df = pd.read_excel(file_path, "Sheet1")
+            return (df.iloc[:, 0].tolist(), df.iloc[:, 1].tolist())
+        except Exception as e:
+            print(f"Error reading Excel file: {e}")
+            return Exception("Error reading Excel file" + str(e))
