d9a5e97517c2c46cee86db4b9851236435aee9ae
Kalana Rubasinghe <kalanarajika99@gmail.com>
2024-09-14 12:30:27 +0530

    json handler

---
+++ JsonHandler.py
@@ -0,0 +1,9 @@
+import json
+
+class JsonHandler:
+    def __init__(self, file_path):
+        self.file_path = file_path
+
+    def write_to_json(self, data):
+        with open(self.file_path, 'w') as file:
+            json.dump(data, file)
